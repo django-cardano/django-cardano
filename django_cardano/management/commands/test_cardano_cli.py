25b9be89fce2a933c213cafafcf31757afc39c94
allan <allan.w.hart@gmail.com>
2021-04-20 10:11:51 -0600

    Added bundling configuration

--- django_cardano/management/commands/test_cardano_cli.py
+++ django_cardano/management/commands/test_cardano_cli.py
@@ -4,3 +4,4 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Hello django-cardano')
+        print('I work alone')
