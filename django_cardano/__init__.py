58f99b05713238254d9c5f5aec76d84ff1d96089
allan <allan.w.hart@gmail.com>
2021-04-23 15:44:37 -0600

    Cleanup

--- django_cardano/__init__.py
+++ django_cardano/__init__.py
@@ -323,24 +323,32 @@ class Cardano:
 
         self._submit_transaction(tx_args, tx_fee, tx_file_directory, from_wallet)
 
-
     def mint_nft(self, asset_name, payment_wallet):
         """
         https://docs.cardano.org/en/latest/native-tokens/getting-started-with-native-tokens.html#start-the-minting-process
         :param payment_wallet: Wallet with sufficient funds to mint the token
         :return:
         """
-        payment_address = payment_wallet.payment_address
-        utxos = self.query_utxos(address=payment_address)
-        if len(utxos) < 1:
-            raise CardanoError(f'Address {payment_address} has no available funds')
-        payment_utxo = utxos[0]
-
-        # 1a. Create a directory to hold intermediate files used to create the transaction
+        #  Create a directory to hold intermediate files used to create the transaction
         tx_file_directory = os.path.join(settings.INTERMEDIATE_FILE_PATH, 'token', str(utcnow().timestamp()))
         os.makedirs(tx_file_directory, 0o755)
 
-        # 1b. Create a minting policy
+        # ALWAYS work with a fresh set of protocol parameters.
+        protocol_parameters = self.refresh_protocol_parameters()
+
+        lovelace_unit = settings.LOVELACE_UNIT
+        min_utxo_value = protocol_parameters['minUTxOValue']
+        payment_address = payment_wallet.payment_address
+        token_lovelace = min_utxo_value * 3
+
+        utxos = self.query_utxos(payment_address)
+        lovelace_utxos = sort_utxos(filter_utxos(utxos, type=lovelace_unit), order='desc')
+        if not lovelace_utxos:
+            # Let there be be at least one UTxO containing purely ADA.
+            # This will be used to pay for the transaction.
+            raise CardanoError(f'Address {payment_address} has inadequate funds to complete transaction')
+
+        # 1. Create a minting policy
         policy_signing_key_path = os.path.join(tx_file_directory, 'policy.skey')
         policy_verification_key_path = os.path.join(tx_file_directory, 'policy.vkey')
         policy_script_path = os.path.join(tx_file_directory, 'policy.script')
@@ -364,15 +372,23 @@ class Cardano:
         })
         mint_argument = f'"1 {policy_id}.{asset_name}"'
 
+        # ASSUMPTION: The largest ADA UTxO shall contain sufficient ADA
+        # to pay for the transaction (including fees)
+        lovelace_utxo = lovelace_utxos[0]
+        total_lovelace_being_sent = lovelace_utxo['Tokens'][lovelace_unit]
+        lovelace_to_return = total_lovelace_being_sent - token_lovelace
+
+        tx_args = [
+            ('tx-in', '{}#{}'.format(lovelace_utxo['TxHash'], lovelace_utxo['TxIx'])),
+            ('tx-out', f'{payment_address}+{token_lovelace}+{mint_argument}'),
+            ('tx-out', f'{payment_address}+{lovelace_to_return}')
+        ]
+
         # 3. Build a draft transaction which will be used to calculate minimum fees
         # https://docs.cardano.org/en/latest/native-tokens/getting-started-with-native-tokens.html#build-the-raw-transaction
         draft_transaction_path = os.path.join(tx_file_directory, 'transaction.draft')
-        tx_in = '{}#{}'.format(payment_utxo['TxHash'], payment_utxo['TxIx'])
-        tx_out = f'{payment_address}+0+{mint_argument}'
-        self.call_cli('transaction build-raw', **{
+        self.call_cli('transaction build-raw', *tx_args, **{
             'mary-era': None,
-            'tx-in': tx_in,
-            'tx-out': tx_out,
             'fee': 0,
             'mint': mint_argument,
             'out-file': draft_transaction_path,
@@ -383,56 +399,17 @@ class Cardano:
         tx_fee = self._calculate_min_fee(**{
             'tx-body-file': draft_transaction_path,
             'tx-in-count': 1,
-            'tx-out-count': 1,
+            'tx-out-count': 2,
             'witness-count': 2,
             'protocol-params-file': self.protocol_parameters_path,
             'network': settings.NETWORK,
         })
 
-        # 5. Build the transaction again, this time including the fee
-        # https://docs.cardano.org/en/latest/native-tokens/getting-started-with-native-tokens.html#build-the-transaction-again
-        # Note that the transaction fee is deducted from the amount ADA being returned
-        raw_transaction_path = os.path.join(tx_file_directory, 'transaction.raw')
-        amount_to_return = payment_utxo[settings.LOVELACE_UNIT] - tx_fee
-        tx_out = f'{payment_address}+{amount_to_return}+{mint_argument}'
-        self.call_cli('transaction build-raw', **{
-            'mary-era': None,
-            'tx-in': tx_in,
-            'tx-out': tx_out,
-            'fee': tx_fee,
-            'mint': mint_argument,
-            'out-file': raw_transaction_path,
-        })
+        # 5. Update the "change" output to deduct the transaction fee
+        # https://docs.cardano.org/projects/cardano-node/en/latest/stake-pool-operations/simple_transaction.html#calculate-the-change-to-send-back-to-payment-addr
+        tx_args[len(tx_args) - 1] = ('tx-out', f'{payment_address}+{lovelace_to_return - tx_fee}')
 
-        # 6. Sign the transaction
-        # https://docs.cardano.org/en/latest/native-tokens/getting-started-with-native-tokens.html#sign-the-transaction
-        signed_transaction_path = os.path.join(tx_file_directory, 'transaction.signed')
-
-        signing_key_file_path = os.path.join(tx_file_directory, 'transaction.skey')
-        with open(signing_key_file_path, 'w') as signing_key_file:
-            json.dump(payment_wallet.payment_signing_key, signing_key_file)
-
-        signing_args = [
-            ('signing-key-file', signing_key_file_path),
-            ('signing-key-file', policy_signing_key_path),
-        ]
-
-        self.call_cli('transaction sign', *signing_args, **{
-            'tx-body-file': raw_transaction_path,
-            'script-file': policy_script_path,
-            'out-file': signed_transaction_path,
-            'network': settings.NETWORK
-        })
-
-        # 7. Submit the transaction
-        # https://docs.cardano.org/en/latest/native-tokens/getting-started-with-native-tokens.html#submit-the-transaction
-        self.call_cli('transaction submit', **{
-            'tx-file': signed_transaction_path,
-            'network': settings.NETWORK
-        })
-
-        # 8. Clean up intermediate files
-        shutil.rmtree(tx_file_directory)
+        self._submit_transaction(tx_args, tx_fee, tx_file_directory, payment_wallet, mint=mint_argument)
 
     # --------------------------------------------------------------------------
     def call_cli(self, command, *args, **kwargs):
@@ -508,7 +485,7 @@ class Cardano:
                 raise CardanoError(source_error=e)
 
     # --------------------------------------------------------------------------
-    def _submit_transaction(self, tx_args, tx_fee, tx_file_directory, wallet):
+    def _submit_transaction(self, tx_args, tx_fee, tx_file_directory, wallet, **kwargs):
         raw_transaction_file = os.path.join(tx_file_directory, 'transaction.raw')
 
         # Determine the TTL (time to Live) for the transaction
@@ -517,27 +494,39 @@ class Cardano:
         current_slot = int(tip['slot'])
         invalid_hereafter = current_slot + settings.DEFAULT_TRANSACTION_TTL
 
-        self.call_cli('transaction build-raw', *tx_args, **{
+        transaction_options = {
             'mary-era': None,
             'invalid-hereafter': invalid_hereafter,
             'fee': tx_fee,
-            'out-file': raw_transaction_file
-        })
+            'out-file': raw_transaction_file,
+            **kwargs,
+        }
+        self.call_cli('transaction build-raw', *tx_args, **transaction_options)
 
         # Sign the transaction
         # https://docs.cardano.org/projects/cardano-node/en/latest/stake-pool-operations/simple_transaction.html#sign-the-transaction
         signed_transaction_path = os.path.join(tx_file_directory, 'transaction.signed')
-        signing_key_file_path = os.path.join(tx_file_directory, 'transaction.skey')
 
-        with open(signing_key_file_path, 'w') as signing_key_file:
-            json.dump(wallet.payment_signing_key, signing_key_file)
-
-        self.call_cli('transaction sign', **{
+        signing_args = []
+        signing_kwargs = {
             'tx-body-file': raw_transaction_file,
-            'signing-key-file': signing_key_file_path,
             'out-file': signed_transaction_path,
             'network': settings.NETWORK
-        })
+        }
+
+        signing_key_file_path = os.path.join(tx_file_directory, 'transaction.skey')
+        with open(signing_key_file_path, 'w') as signing_key_file:
+            json.dump(wallet.payment_signing_key, signing_key_file)
+        signing_args.append(('signing-key-file', signing_key_file_path))
+
+        if 'mint' in kwargs:
+            policy_signing_key_path = os.path.join(tx_file_directory, 'policy.skey')
+            signing_args.append(('signing-key-file', policy_signing_key_path))
+
+            policy_script_path = os.path.join(tx_file_directory, 'policy.script')
+            signing_kwargs['script-file'] = policy_script_path
+
+        self.call_cli('transaction sign', *signing_args, **signing_kwargs)
 
         # Submit the transaction
         # https://docs.cardano.org/projects/cardano-node/en/latest/stake-pool-operations/simple_transaction.html#submit-the-transaction
